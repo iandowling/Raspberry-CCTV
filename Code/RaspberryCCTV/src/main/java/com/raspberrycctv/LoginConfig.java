bd7e0418e3494b42991bb9f1e8116c27bf5bebde
iandowling <iandowling3@gmail.com>
2015-12-17 21:26:12 +0000

    Committing basic version of user login with Spring/Maven.

---
+++ Code/RaspberryCCTV/src/main/java/com/raspberrycctv/LoginConfig.java
@@ -0,0 +1,18 @@
+package com.raspberrycctv;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class LoginConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
